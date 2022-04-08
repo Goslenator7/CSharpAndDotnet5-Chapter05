79da19fcc9fc2c7c90be72d29d6355d9ae4705ad
ryan <ryancumming7@gmail.com>
2022-04-08 13:33:03 +0100

    Add project files.

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Packt.Shared
+{
+    public class Person : object
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public List<Person> Children = new List<Person>();
+
+    }
+}
---
+++ PeopleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = new Person();
+            bob.Name = "Bob Smith";
+            bob.DateOfBirth = new DateTime(1965,12,22);
+            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            bob.Children.Add(new Person {Name = "Alfred" });
+            bob.Children.Add(new Person {Name = "Zoe" });
+
+            WriteLine($"{bob.Name} has {bob.Children.Count} children: ");
+
+            for (int child = 0; child < bob.Children.Count; child++)
+            {
+                WriteLine($"{bob.Children[child].Name}");
+            }
+
+            //WriteLine(
+                //format: "{0}'s favorite wonder is {1}. Its integer is {2}",
+                //arg0 : bob.Name,
+                //arg1 : bob.FavoriteAncientWonder,
+                //arg2 : (int)bob.FavoriteAncientWonder);
+
+            var alice = new Person
+            {
+                Name = "Alice Jones",
+                DateOfBirth = new DateTime(1998, 3, 7)
+            };
+
+            WriteLine($"{alice.Name} was born on {alice.DateOfBirth}");
+        }
+    }
+}
